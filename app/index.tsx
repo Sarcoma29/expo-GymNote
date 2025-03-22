85fa769c427ee69c1ab6b38e7bbc16822b513c76
sarcoma29 <soroin.georiy@yandex.ru>
2025-03-22 18:26:52 +0300

    first build

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import { Text, View } from "react-native";
+import {useState, useEffect} from "react";
+import { Text, View, Button } from "react-native";
 import styled from 'styled-components/native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -7,48 +7,50 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import MyButton from "@/components/myButton";
 
-export default function Index() {
-
-  const StyledHeader = styled.View`
+const StyledHeader = styled.View`
     padding: 10px;
     margin: 10px;
     display: flex;
     flex-direction: row;
   `
 
+export default function Index() {
+
   const [count, setCount] = useState(0);
 
-  const storeData = async (value : string) => {
-    try {
-      await AsyncStorage.setItem('counter', value)
-    } catch (e) {
-      console.log(e)
+
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let value = await AsyncStorage.getItem('myAppCounter')
+        if (value !== null) {
+          setCount(JSON.parse(value))
+          console.log("данные получены")
+        }
+      } catch (e) {
+        console.log(" error getting counter in DATA", e)
+      }  
     }
-  }
-  
+    getData()
+  }, [])
 
-  function HandleClick() {
-    setCount(count + 1);
-    storeData(JSON.stringify(count))
-  }
-
-
-
-
-  const getData = async () => {
-    try {
-      let value = await AsyncStorage.getItem('counter')
-      if (value !== null) {
-        setCount(JSON.parse(value))
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        await AsyncStorage.setItem('myAppCounter', JSON.stringify(count))
+        console.log("данные записаны")
+      } catch (e) {
+        console.log("error setter counter in DATA", e)
       }
-    } catch (e) {
-      console.log(" error getting counter in DATA")
-    }
+    };
+    storeData()
+  }, [count]);
+
+  function handleClick() {
+    setCount(count + 1);
   }
 
-
-
-  
   return (
     <View
       style={{
@@ -60,12 +62,22 @@ export default function Index() {
       <Text>Click clack test million ready</Text>
 
         <StyledHeader>
-          <MyButton count={count} onClick={HandleClick} />
-          <MyButton count={count} onClick={HandleClick} />
-          <MyButton count={count} onClick={HandleClick} />
+          <MyButton count={count} onClick={handleClick} />
+          <MyButton count={count} onClick={handleClick} />
+          <MyButton count={count} onClick={handleClick} />
         </StyledHeader>
 
+        <Button
+          title="Проверить хранилище"
+          onPress={async () => {
+            const value = await AsyncStorage.getItem('counter');
+            console.log('Текущее значение в хранилище:', value);
+          }}
+        />
+
       </View>
+
+
       
   );
 }
